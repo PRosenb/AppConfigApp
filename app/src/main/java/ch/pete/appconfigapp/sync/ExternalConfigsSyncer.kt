a2f59d493d1d0fa682b1f450fafd711dad768de5
Peter Rosenberg <p.rosenberg@gmx.net>
2020-07-12 10:10:31 +0200

    handle cleartext and wrong URL exception

--- app/src/main/java/ch/pete/appconfigapp/sync/ExternalConfigsSyncer.kt
+++ app/src/main/java/ch/pete/appconfigapp/sync/ExternalConfigsSyncer.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import retrofit2.HttpException
 import timber.log.Timber
+import java.net.UnknownServiceException
 import java.util.Calendar
 
 class ExternalConfigsSyncer(
@@ -75,6 +76,14 @@ class ExternalConfigsSyncer(
             e.printStackTrace()
             0
         } catch (e: HttpException) {
+            Timber.e(e, "Could not fetch external config location %s", externalConfigLocation)
+            e.printStackTrace()
+            0
+        } catch (e: UnknownServiceException) {
+            Timber.e(e, "Could not fetch external config location %s", externalConfigLocation)
+            e.printStackTrace()
+            0
+        } catch (e: IllegalArgumentException) {
             Timber.e(e, "Could not fetch external config location %s", externalConfigLocation)
             e.printStackTrace()
             0
